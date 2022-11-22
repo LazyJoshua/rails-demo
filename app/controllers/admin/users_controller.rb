3bf9d6404e16f84c3dff49b205dd95e4379518f5
LazyJoshua <568178051@qq.com>
2022-11-22 15:50:45 +0800

    后台管理layout

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,7 @@
 class Admin::UsersController < ApplicationController
+
+    layout 'admin'
+
     def index
         @users = User.page(params[:page] || 1).per_page(params[:per_page] || 5).order("id desc")
     end
